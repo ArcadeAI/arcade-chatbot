5c5b0ce09ab44800f177cb046c985eb5a5eaa50a
Sam Partee <Partees21@gmail.com>
2024-11-15 18:02:28 -0800

    Update to use github auth

--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
-const nextConfig = {
+const webpack = require('webpack');
+
+/** @type {import('next').NextConfig} */
+module.exports = {
   images: {
     remotePatterns: [
       // Add remote patterns here
     ],
   },
+  env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+    ARCADE_API_KEY: process.env.ARCADE_API_KEY,
+    ARCADE_ENGINE_URL: process.env.ARCADE_ENGINE_URL,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+      };
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          crypto: 'crypto-browserify',
+        })
+      );
+    }
+    return config;
+  },
 };
 
-module.exports = nextConfig;
