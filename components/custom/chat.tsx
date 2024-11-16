0245b30641197e9e0ab1fa157ae025fc97818fde
Sergio Serrano <sdserranog@gmail.com>
2024-11-16 01:02:54 -0400

    Fix overflow in mobile

--- components/custom/chat.tsx
+++ components/custom/chat.tsx
@@ -110,7 +110,7 @@ export function Chat({
         <ChatHeader selectedModelId={selectedModelId} />
         <div
           ref={messagesContainerRef}
-          className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4 break-words"
+          className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4 [overflow-wrap:anywhere] [word-break:break-word]"
         >
           {messages.length === 0 && <Overview />}
 
